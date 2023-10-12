ae05acf59dea18f32d40f5e9876def316e9a4d8f
HungQuocDang <hungquoc016@gmail.com>
2023-10-12 15:47:01 -0400

    updates

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -43,7 +43,7 @@ function Nav() {
     <header className="flex-row px-1">
       <h1>
         <Link to="/">
-          {/* <span role="img" aria-label="shopping bag">🛍️   🛍️ </span> */}
+         
           <span role="img" aria-label="shopping bag">   👀...👀...  </span>
           Happy-Feet-Shop ...👀...👀
         </Link>
